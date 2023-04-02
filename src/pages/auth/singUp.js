a520b9fe25d8ebdafac3bd4f7a0f462d6b79c42f
Satyam7Jha <75986382+Satyam7Jha@users.noreply.github.com>
2023-04-03 03:24:28 +0530

    update. button type

--- src/pages/auth/singUp.js
+++ src/pages/auth/singUp.js
@@ -26,7 +26,7 @@ export default function Signup() {
   const handleSubmit = async(e) => {
     e.preventDefault();
     console.log("dfjdlkfjdlkfj")
-    await addUser(userDetails)
+  
   };
 
   //handle Signup API Integration here
@@ -92,6 +92,7 @@ export default function Signup() {
             <FormAction handleSubmit={handleSubmit} text="Signup" href={'/home'}/>
           ) : (
             <button
+              type='button'
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 mt-10"
               onClick={createAccount}
             >
