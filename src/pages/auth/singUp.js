9c73a5f56b1e5280cdf0fb40e4a2401d0e2be721
Satyam7Jha <75986382+Satyam7Jha@users.noreply.github.com>
2023-04-03 03:38:02 +0530

    revove. button

--- src/pages/auth/singUp.js
+++ src/pages/auth/singUp.js
@@ -31,7 +31,7 @@ export default function Signup() {
 
   //handle Signup API Integration here
   const createAccount = (e) => {
-    e.preventDefault();
+    
     signInWithPopup(auth, provider)
       .then((result) => {
         // This gives you a Google Access Token. You can use it to access the Google API.
@@ -92,13 +92,13 @@ export default function Signup() {
           {toggle ? (
             <FormAction handleSubmit={handleSubmit} text="Signup" href={'/home'}/>
           ) : (
-            <button
+            <div
               type='button'
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 mt-10"
               onClick={createAccount}
             >
               signUp with google
-            </button>
+            </div>
           )}
         </div>
       </form>
