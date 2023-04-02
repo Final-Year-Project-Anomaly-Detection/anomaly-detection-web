ee5eb9a2b67510dcce50e134a5d9c1724f731fdb
Satyam7Jha <75986382+Satyam7Jha@users.noreply.github.com>
2023-04-03 00:02:10 +0530

    Add. Login and signUp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import LoginPage from "./pages/auth/LoginPage"
+import SignUpPage from "./pages/auth/SignUpPage"
+import Home from './pages/home';
 
-export default function App() {
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      React app
-    </h1>
-  )
+   <div>
+     <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<LoginPage/>} />
+            <Route path="/signup" element={<SignUpPage/>} />
+            <Route path="/home" element={<Home/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+
+  );
 }
+
+export default App;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+  const Home = ()=>{
+    return(<div className='bg-red-500 min-h-full h-screen min-w-full w-screen'>Home</div>)
+}
+export default Home;
